34cd22b4a03855aeecc51dece073a2c1f90c4728
Vinill <danimuller758@gmail.com>
2022-09-06 21:01:53 +0000

    pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,19 +7,19 @@ import json
 import requests
 
 if __name__ == "__main__":
-    get__user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+    get__user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(argv[1]))
     get__name = (get__user.json().get("name"))
-    var = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    #print(var.json())
+    var = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                       .format(argv[1]))
     get__val = var.json()
-    #print(get__val)
     get__len__val = len(get__val)
-    #print(get__len__val)
     res = []
     count = 0
     for task in get__val:
         if task["completed"] is True:
             res.append(task.get("title"))
-            count =+ 1
+            count += 1
+
     for title in res:
-        print("\t {}".format(title)) 
+        print("\t {}".format(title))
