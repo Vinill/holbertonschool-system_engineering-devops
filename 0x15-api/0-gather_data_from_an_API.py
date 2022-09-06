83e4814afeec07427059c81cb5c3eb2ab8b2051d
Vinill <danimuller758@gmail.com>
2022-09-06 23:55:54 +0000

    hola

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,8 +2,8 @@
 """Write a Python script that, using this REST API, for a given employee ID
 returns information about his/her TODO list progress"""
 
-import requests
 from sys import argv
+import requests
 import json
 
 if __name__ == "__main__":
