7b59c60e49e541fc40bb2904036df389c0486afa
Vinill <danimuller758@gmail.com>
2022-09-21 21:34:37 +0000

    ejercices 0 and 1 with pycod fix

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API
+and returns the number of subscribers
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    subreds = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    red_req = requests.get(subreds, headers={'User-Agent': 'hola'})
+    if (red_req.status_code == requests.codes.ok):
+        jason_full = red_req.json()
+        return(jason_full['data']['subscribers'])
+    else:
+        return (0)
