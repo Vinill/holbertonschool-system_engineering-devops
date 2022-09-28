fe176ebee4688e31d3beea9307d8f9c352170146
Vinill <danimuller758@gmail.com>
2022-09-28 14:46:22 +0000

    fix 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,3 @@
 # Puppet script that Change the OS configuration so that it is possible
 # to login with the holberton user and open a file without any error message.
-exec {
-    'sed -i "s/holberton/foo/" /etc/security/limits.conf':
-    }
+exec {'sed -i "s/holberton/foo/" /etc/security/limits.conf':}
